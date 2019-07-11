cd3b8a67fd0c8477ab89615a76796d7707a9d66b
horwardL <hwloh14@gmail.com>
2019-07-11 18:18:41 -0400

    train multiple pic and predict the face on webcame

---
+++ face_recog.py
@@ -0,0 +1,116 @@
+import face_recognition
+import cv2
+import numpy as np
+from sklearn import svm
+import os
+
+img_counter = 0
+video_capture = cv2.VideoCapture(0)
+
+# Load a sample picture and learn how to recognize it.
+# obama_image = face_recognition.load_image_file("obama.jpg")
+# obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# The training data would be all the face encodings from all the known images and the labels are their names
+encodings = []
+names = []
+
+# Training directory
+train_dir = os.listdir('train_dir')
+
+# Loop through each person in the training directory
+for person in train_dir:
+    pix = os.listdir("train_dir/" + person)
+
+    # Loop through each training image for the current person
+    for person_img in pix:
+        # Get the face encodings for the face in each image file
+        face = face_recognition.load_image_file("train_dir/" + person + "/" + person_img)
+        face_bounding_boxes = face_recognition.face_locations(face)
+
+        #If training image contains none or more than faces, print an error message and exit
+        if len(face_bounding_boxes) != 1:
+            print(person + "/" + person_img + " contains none or more than one faces and can't be used for training.")
+            exit()
+        else:
+            face_enc = face_recognition.face_encodings(face)[0]
+            # Add face encoding for current image with corresponding label (name) to the training data
+            encodings.append(face_enc)
+            names.append(person)
+
+clf = svm.SVC(gamma='scale')
+clf = clf.fit(encodings, names)
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [encoding for encoding in encodings]
+known_face_names = [name for name in names]
+
+# Initialize some variables
+face_locations = []
+face_encoding = []
+face_names = []
+process_this_frame = True
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image face_recognitionom BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = clf.predict([face_encoding])
+
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
+
+            # Or instead, use the known face with the smallest distance to the new face
+            #face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            #best_match_index = np.argmin(face_distances)
+            #if matches[best_match_index]:
+            #   name = known_face_names[best_match_index]
+
+            face_names.append(*name)
+
+    process_this_frame = not process_this_frame
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
